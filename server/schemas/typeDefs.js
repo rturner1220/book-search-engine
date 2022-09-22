8215c533c58e83c29dc680ade6e895bba68dfbed
Rosa Turner <rmturner1220@gmail.com>
2022-09-22 14:15:07 -0400

    schemas update

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,42 @@
 // import the gql tagged template function
-const { gql } = require('apollo-server-express');
+const { gql } = require
+    ('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
 type Query {
-    helloWorld: String
-  }`;
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  `;
 
 // export the typeDefs
 module.exports = typeDefs;
